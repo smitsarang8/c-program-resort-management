1b1b4418af427756a0e72cd5f919f61ce8ac1505
Smit Sarang <smitsarang86@gmail.com>
2021-02-10 23:18:00 +0530

    Update sourceCode.c

--- sourceCode.c
+++ sourceCode.c
@@ -1,341 +1,302 @@
-/*
-*/
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <stdlib.h>
-#define MAXID 10
 #define MAXNAME 25
-#define MAXPRICE 10
-
-//entity of admin
-struct admin {
-    char username[20];
-    char password[20];
+#define MAXCODE 25
+FILE *fp, *ft;
+​
+//smitStarts
+struct admin
+{
+     char username[20];
+     char password[20];
 };
-struct admin admin_user1 = {"admin","welcome1"};
-
-//entity of service
+struct admin a1 = {"admin", "welcome1"};
+struct admin a2 = {"customer", "welcome1"};
+​
 struct service
 {
-    char id[MAXID];
-    char name[MAXNAME];
-    char price[MAXPRICE];
+     char code[MAXCODE];
+     char name[MAXNAME];
+     float price;
 } s1;
-
-//smsarang
+//smitEnds
+​
+char servicename[MAXNAME];
+int found = 0;
+long int recsize=sizeof(s1);
+​
+//smitStarts
 void checkSystemFile();
-void createAuthenticationCsv(int authenticated,char *filename,char userinputusername[],char userinputpassword[]);
-void createServicesCsv(char *filename, char userinputusername[]);
-void readServicesCsv(char *filename, char searchQ[]);
-void updateServicepriceCsv(char *filename, char recordNo[], char newPrice[]);
-void generateBakupFileOfAll();
-
-/*
-To check if required files are created or not to store information.
-@param char pointer variable filename
-author : smit
-*/
-void checkSystemFile(){
-    FILE *fp;
-    char *filename="datafile_authenticationlogs.csv";
-    printf("\n\n!!!Setting up the required files...\n");
-    printf("\nChecking %s", filename);
-
-    fp = fopen(filename, "r");
-    if (!fp){
-        printf("\nFile not found, Creating new\n");
-        fp = fopen(filename, "w");
-        fprintf(fp, "username,password,authentication");
-        fclose(fp);
-        printf("\nfile %s checked...\n",filename);
-    }else{
-        printf("\nfile %s checked...\n",filename);    
-    }
-    
-    filename="datafile_services.csv";
-    printf("\nChecking %s", filename);
-
-    fp = fopen(filename, "r");
-    if (!fp){
-        printf("\nFile not found, Creating new\n");
-        fp = fopen(filename, "w");
-        fprintf(fp, "id,name,price,addedby");
-        fclose(fp);
-        printf("\nfile %s checked...\n",filename);
-    }else{
-        printf("\nfile %s checked...\n",filename);    
-    }
-}
-
-/*
-To store authentication logs into a file.
-@param int authenticated
-@param char filename
-@param char userinputusername
-@param char userinputpassword
-author : smit
-*/
-void create_authentication_csv(int authenticated,char *filename,char userinputusername[],char userinputpassword[]){
-    FILE *fp;
-    filename = strcat(filename, ".csv");
-
-    fp = fopen(filename, "r");
-    if (!fp){
-        printf("\nFile %s not found. \nPlease setup the required files\n",filename);
-        printf("execute program using 'syscheck' as cli arg\n\n");
-        exit(1);
-    }else{
-        fclose(fp);
-        fp = fopen(filename, "a");
-    }
-
-    if(authenticated==1){
-        fprintf(fp, "\n%s,%s,%s", userinputusername, userinputpassword, "Success");
-    }else{
-        fprintf(fp, "\n%s,%s,%s", userinputusername, userinputpassword, "Failed");
-    }
-    fclose(fp);
-}
-
-/*
-To create services
-@param char pointer filename
-@param char userinputusername
-author : smit
-*/
-void createServicesCsv(char *filename, char userinputusername[])
+// void createAuthentication(int authenticated,char *filename,char userinputusername[],char userinputpassword[]);
+void createServices(char *filename, char userinputusername[]);
+void readServices(char *filename);
+void updateServices(char *filename);
+void deleteServices(char *filename);
+// void generateBakupFileOfAll();
+void flush();
+​
+void flush()
 {
-    FILE *fp;
-    int i, count;
-    filename = strcat(filename, ".csv");
-
-    fp = fopen(filename, "r");
-    if (!fp)
-    {
-        printf("\nFile %s not found. \nPlease setup the required files\n",filename);
-        printf("execute program using 'syscheck' as cli arg\n\n");
-        exit(1);
-    }
-    else
-    {
-        fp = fopen(filename, "a");
-    }
-
-    printf("\nHow many rows?");
-    scanf("%d", &count);
-
-    for (i = 1; i <= count; i++)
-    {
-        printf("\nEnter id: ");
-        scanf("%s", s1.id);
-        printf("\nEnter name: ");
-        scanf("%s", s1.name);
-        printf("\nEnter price: ");
-        scanf("%s", s1.price);
-        fprintf(fp, "\n%s,%s,%s,%s", s1.id, s1.name, s1.price,userinputusername);
-    }
-    fclose(fp);
+     int c;
+     while ((c = getchar()) != '\n' && c != EOF);
 }
-
-
-/*
-To read services
-@param char pointer filename
-@param char searchQ
-author : smit
-*/
-void readServicesCsv(char *filename, char searchQ[])
+​
+void createServices(char *filename, char userinputusername[])
 {
-    char temp[100];
-    char lines[100];
-    FILE *fp;
-    int j;
-    filename = strcat(filename, ".csv");
-    fp = fopen(filename, "r");
-    if (!fp)
-    {
-        printf("\nFile %s not found. \nPlease setup the required files\n",filename);
-        printf("execute program using 'syscheck' as cli arg\n\n");
-        exit(1);
-    }else{
-            printf("\n\n\t++ Services Data ++\n\n");
-    }
-
-    while (fgets(lines, sizeof(lines), fp))
-    {
-        char *token;
-        token = strtok(lines, ",");
-        while (token != NULL)
-        {
-            printf("\t%s", token);
-            token = strtok(NULL, ",");
-            j++;
-        }
-        printf("\n");
-    }
-    // printf("\n\tRecords %s------\n", lines);
-    fclose(fp);
-    printf("\n");
+     int isExists = 0;
+     fp = fopen(filename, "r+");
+     fflush(stdin);
+     printf("\t\tEnter the service name you want to create: ");
+     scanf("%s", servicename);
+     rewind(fp);
+     while (fread(&s1, recsize, 1, fp) > 0 && found == 0)
+     {
+          if (strcmp(s1.name, servicename) == 0)
+          {
+               isExists = 1;
+​
+          }
+     }
+     if (!isExists)
+     {
+          fseek(fp, 0, SEEK_END);
+          flush();
+          printf("\n\t\tEnter service code: ");
+          scanf("%s", s1.code);
+          fflush(stdin);
+          printf("\n\t\tEnter name: ");
+          scanf("%s", s1.name);
+          fflush(stdin);
+          printf("\n\t\tEnter price: ");
+          scanf("%f", &s1.price);
+          fflush(stdin);
+          fwrite(&s1, recsize, 1, fp);
+     }
+     else
+     {
+          printf("\t\tRecord already exists with name %s\n", servicename);
+     }
+     fclose(fp);
 }
-
-/*
-To take backup of all the data files.
-author : smit
-*/
-void generateBakupFileOfAll(){
-    time_t t;
-    time(&t);
-    printf("\n!!!generating backup as per %s\n", ctime(&t));
+​
+void readServices(char *filename)
+{
+     printf("\t\t++++ Services ++++\n");
+     printf("\n\t\tCODE\t\tNAME\t\tPRICE\n\n");
+     fp = fopen(filename, "r+");
+     rewind(fp);
+     while (fread(&s1, recsize, 1, fp) == 1)
+     {
+          printf("\t\t%s\t\t%s\t\t%.2f\n", s1.code, s1.name, s1.price);
+     }
+     fclose(fp);
 }
-
-
-// void updateServicepriceCsv(char *filename, char recordNo[], char newPrice[])
-// {
-//     char temp[100];
-//     char rowData[100];
-//     printf("\n\n\t++ Row %s Data ++\n\n", recordNo);
-//     FILE *fp;
-//     int cntr, k;
-//     filename = strcat(filename, ".csv");
-//     fp = fopen(filename, "r+");
-//     char lines[100];
-//     while (fgets(lines, sizeof(lines), fp))
-//     {
-//         char *token;
-//         token = strtok(lines, ",");
-//         int i = 0;
-//         while (token != NULL)
-//         {
-//             fscanf(fp, "%[^\n]", rowData);
-//             if (strcmp(lines, "2") == 0)
-//             {
-//                 printf("\t%s", token);
-//                 cntr++;
-//             }
-//             token = strtok(NULL, ",");
-//             i++;
-//         }
-//     }
-//     printf("\n");
-//     printf("Data from the file:\n%s", rowData);
-//     printf("\n-----%d\n", cntr);
-//     fclose(fp);
-
-//     int j = 0;
-//     char *p = strtok(rowData, ",");
-//     char *newArray[3];
-
-//     while (p != NULL)
-//     {
-//         newArray[j++] = p;
-//         p = strtok(NULL, ",");
-//     }
-
-//     // printf("old val");
-//     // for (j = 0; j < 3; ++j)
-//     // {
-//     //     printf("\n%s", newArray[j]);
-//     // }
-
-//     for (int i = 0; i < 3; i++)
-//     {
-//         if (i == 2)
-//         {
-//             newArray[i] = newPrice;
-//         }
-//     }
-
-//     printf("new val");
-//     for (j = 0; j < 3; ++j)
-//     {
-//         printf("\n%s", newArray[j]);
-//     }
-
-//     char ch, source_file[50] = "datafile_services.csv", target_file[50] = "datafile_services copy.csv";
-//     FILE *source, *target;
-
-//     source = fopen(source_file, "r");
-//     if (source == NULL)
-//     {
-//         printf("Source File not found...\n");
-//     }
-
-//     target = fopen(target_file, "w");
-//     if (target == NULL)
-//     {
-//         fclose(source);
-//         printf("Target File not found...\n");
-//     }
-//     fp = fopen(filename, "r");
-//     char c;
-//     int counter=1;
-//     for (c = getc(fp); c != EOF; c = getc(fp))
-//     {
-//         if (c == '\n')
-//         {
-//             counter = counter + 1;
-//         }
-//     }
-
-//     printf("File will be copied .\n");
-//     printf("%d record will be copied.\n", counter);
-
-// }
-
+void searchAndGet(char *filename)
+{
+     int isExists = 0;
+     fp = fopen(filename, "r+");
+     fflush(stdin);
+     printf("\t\tEnter the service name you want to search: ");
+     scanf("%s", servicename);
+     rewind(fp);
+     while (fread(&s1, recsize, 1, fp) > 0 && found == 0)
+     {
+          if (strcmp(s1.name, servicename) == 0)
+          {
+               isExists = 1;
+               printf("Service Name : %s",s1.name);
+               printf("Service Code : %s",s1.code);
+               printf("\nService Cost : %s",s1.price);
+​
+          }
+     }
+}
+​
+void updateServices(char *filename)
+{
+     int isExists = 0;
+     fp = fopen("datafile_service.txt", "r+");
+     fflush(stdin);
+     printf("\t\tEnter the service name to modify: ");
+     scanf("%s", servicename);
+     rewind(fp);
+     while (fread(&s1, recsize, 1, fp) > 0 && isExists == 0)
+     {
+          if (strcmp(s1.name, servicename) == 0)
+          {
+               isExists = 1;
+               printf("\n\t\tEnter new code: ");
+               scanf("%s", s1.code);
+               printf("\n\t\tEnter new name: ");
+               scanf("%s", s1.name);
+               fflush(stdin);
+               printf("\n\t\tEnter new price: ");
+               scanf("%f", &s1.price);
+               fseek(fp, -recsize, SEEK_CUR);
+               fwrite(&s1, recsize, 1, fp);
+               break;
+          }
+     }
+     if(!isExists){
+          printf("\t\tEditing failed \n\t\tRecord does not exists with name %s\n", servicename);
+     }
+     found = 0;
+     fclose(fp);
+}
+​
+void deleteServices(char *filename)
+{
+     int isExists = 0;
+     fp = fopen(filename, "r+");
+     fflush(stdin);
+     printf("\t\tEnter the service name you want to delete: ");
+     scanf("%s", servicename);
+     rewind(fp);
+     while (fread(&s1, recsize, 1, fp) > 0 && found == 0)
+     {
+          if (strcmp(s1.name, servicename) == 0)
+          {
+               isExists = 1;
+          }
+     }
+     if (isExists)
+     {
+          flush();
+          ft = fopen("Tdatafile_service.txt", "w");
+          rewind(fp);
+          while (fread(&s1, recsize, 1, fp) == 1)
+          {
+               if (strcmp(s1.name, servicename) != 0)
+               {
+                    fwrite(&s1, recsize, 1, ft);
+               }
+          }
+          fclose(fp);
+          fclose(ft);
+          remove("datafile_service.txt");
+          rename("Tdatafile_service.txt", "datafile_service.txt");
+          printf("Record deleted successfully\n");
+          fp = fopen("datafile_service.txt", "r+");
+          fclose(fp);
+     }else{
+          printf("\t\tDelete failed \nRecord does not exists with name %s\n", servicename);
+     }
+}
+​
+void checkSystemFile()
+{
+     FILE *fp;
+     char *filename = "datafile_service.txt";
+​
+     fp = fopen(filename, "r+");
+     if (!fp)
+     {
+          printf("\n\t\tFile not found, Creating new\n");
+          fp = fopen(filename, "w+");
+          //    fprintf(fp, "username,password,authentication");
+          fclose(fp);
+     }
+     fclose(fp);
+}
+//smitEnds
+​
 int main(int cli_params, char *args[])
 {
-    char filename_services[50] = "datafile_services";
-    char filename_authenticaction[50]="logfile_authentication";
-    int authenticated=1;
-    // char userinputusername[20] = "dummy";
-    // char userinputpassword[20];
-    if(cli_params>1){
-        if(!strcmp(args[1],"syscheck")){
-            checkSystemFile();
-        }
-        exit(1);
-    }
-    
-    // printf("\nEnter username: ");
-    // gets(userinputusername);
-    // printf("\nEnter password: ");
-    // gets(userinputpassword);
-
-    // if(strcmp(admin_user1.username,userinputusername)==0 && strcmp(admin_user1.password,userinputpassword)==0){
-    //     authenticated=1;
-    //     createAuthenticationCsv(authenticated, filename_authenticaction,userinputusername,userinputpassword);
-    // }else{
-    //     createAuthenticationCsv(authenticated, filename_authenticaction,userinputusername,userinputpassword);
-    // }
-    
-
-    if(authenticated){
-        int choice;
-        printf("Choose:");
-        printf("\n1.Create service");
-        printf("\n2.View all services");
-        printf("\n3.Generate backup file");
-        printf("\nans:");
-        scanf("%d",&choice);
-        switch (choice)
-        {
-        case 1:
-            createServicesCsv(filename_services, "dummyuser");
-            break;
-        case 2:
-            readServicesCsv(filename_services, "usernameofadmin");
-            break;
-        case 3:
-            generateBakupFileOfAll();
-        default:
-            break;
-        }
-    // updateServicepriceCsv(filename_services, "2", "999");
-    // generateBakupFileOfAll();
-    }else{
-        printf("\nAuth failed");
-    }
-    return 0;
+     //smitStarts
+     char choice;
+     char service_file_name[40] = "datafile_service.txt";
+     char userinputusername[40], userinputpassword[40];
+     int adminauthenticated, userauthenticated = 0;
+     //smitEnds
+​
+     //smitStarts
+     if (cli_params > 1)
+     {
+          if (!strcmp(args[1], "syscheck"))
+          {
+               checkSystemFile();
+          }
+          exit(1);
+     }
+     
+     checkSystemFile();
+​
+     fp = fopen(service_file_name, "r+");
+     if (fp == NULL)
+     {
+          fclose(fp);
+          fp = fopen(service_file_name, "w+");
+          if (fp == NULL)
+          {
+               printf("\t\tConnot create service file");
+               exit(1);
+               fclose(fp);
+          }
+          fclose(fp);
+     }
+     //smitEnds
+​
+     printf("\nEnter username: ");
+     gets(userinputusername);
+     // printf("Enter password: ");
+     // gets(userinputpassword);
+​
+     // if (strcmp(a1.username, userinputusername) == 0 && strcmp(a1.password, userinputpassword) == 0)
+     // {
+     //      adminauthenticated = 1;
+     // }
+     // else if (strcmp(a2.username, userinputusername) == 0 && strcmp(a2.password, userinputpassword) == 0)
+     // {
+     //      userauthenticated = 1;
+     // }
+     // else
+     // {
+     //      printf("Authentication failed\n");
+     // }
+​
+     // if (adminauthenticated == 1 && strcmp(userinputusername, "admin") == 0)
+     // {
+     //smitStarts
+     while(1){
+     printf("\nHi,%s\n", userinputusername);
+     printf("\nWhat do you want to do ?\n");
+     printf("1. Add service\n");
+     printf("2. List services\n");
+     printf("3. Modify service\n");
+     printf("4. Delete service\n");
+     printf("5. Search service\n");
+     printf("6. Exit\n");
+     printf("Choice: ");
+     fflush(stdin);
+     scanf("\n%c", &choice);
+     switch (choice)
+     {
+     case '1':
+          createServices(service_file_name, "dummyuser");
+          break;
+     case '2':
+          readServices(service_file_name);
+          break;
+     case '3':
+          updateServices(service_file_name);
+          break;
+     case '4':
+          deleteServices(service_file_name);
+          break;
+     case '5':
+          searchAndGet(service_file_name);
+     case '6':
+          fclose(fp);
+          exit(0);
+     }
+     }
+     //smitEnds
+     // }
+     // if (userauthenticated && strcmp(userinputusername, "customer") == 0)
+     // {
+     //      printf("\nWelcome %s\n", userinputusername);
+     // }
+     return 0;
 }
